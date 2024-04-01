c6dbd9442d6bb48fe4c25bf609df76d2d58e7849
LEVIS <levisrabah@gmail.com>
2024-04-01 17:56:40 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+
+const testVar = "hi"
+
+//  The code that removes the node element  
+
+document.querySelector("main#main").remove();
+
+// Create a new element and assign it to a newHeader
+
+const newHeader = document.createElement("h1");
+
+newHeader.id = "victory"
+
+newHeader.innerHTML = "Levis Rabah is the champion!";
+
+newHeader.className = "victory";
